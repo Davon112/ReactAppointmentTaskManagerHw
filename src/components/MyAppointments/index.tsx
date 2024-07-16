7ea5bf5003c49bf2373267d334a04488970afad1
Da'Von Manuel <160780050+Davon112@users.noreply.github.com>
2024-07-16 09:55:55 -0500

    Update index.tsx

--- src/components/MyAppointments/index.tsx
+++ src/components/MyAppointments/index.tsx
@@ -1,39 +1,37 @@
-
 import React from 'react';
 
 export interface Appointment {
-    date: string;
-    type: string;
-    doctor: string;
-    patientInfo: string;
-    labs: string;
-    medication: string;
-    height: string;
-    weight: string;
-    vaccinated: string;
-    taxiService: string;
-  }
+  date: string;
+  type: string;
+  doctor: string;
+  patientInfo: string;
+  labs: string;
+  medication: string;
+  height: string;
+  weight: string;
+  vaccinated: string;
+  taxiService: string;
+}
 
-  interface AppointmentCardProps {
+interface AppointmentCardProps {
   appointment: Appointment;
-  bgColor: String;
-  }
-  
+  bgColor: string;
+}
 
 const AppointmentCard: React.FC<AppointmentCardProps> = ({ appointment, bgColor }) => {
   return (
-    <div className={`card ${bgColor} order-card`}>
-      <div className="card-block">
-        <h2 className="m-b-20">{appointment.date}</h2>
-        <h4 className="text-right"><i className="appointment1"></i><span>{appointment.type}</span></h4>
-        <p className="m-b-0">Doctor: {appointment.doctor}</p>
-        <p className="m-b-0">{appointment.patientInfo}</p>
-        <p className="m-b-0">Labs: {appointment.labs}</p>
-        <p className="m-b-0">Medication: {appointment.medication}</p>
-        <p className="m-b-0">Height: {appointment.height}</p>
-        <p className="m-b-0">Weight: {appointment.weight}</p>
-        <p className="m-b-0">Vaccinated: {appointment.vaccinated}</p>
-        <p className="m-b-0">Used Taxi Service: {appointment.taxiService}</p>
+    <div className={`card text-white ${bgColor} mb-3`}>
+      <div className="card-header">{appointment.type}</div>
+      <div className="card-body">
+        <h5 className="card-title">Appointment Date: {appointment.date}</h5>
+        <p className="card-text"><strong>Doctor:</strong> {appointment.doctor}</p>
+        <p className="card-text"><strong>Patient Info:</strong> {appointment.patientInfo}</p>
+        <p className="card-text"><strong>Labs:</strong> {appointment.labs}</p>
+        <p className="card-text"><strong>Medication:</strong> {appointment.medication}</p>
+        <p className="card-text"><strong>Height:</strong> {appointment.height}</p>
+        <p className="card-text"><strong>Weight:</strong> {appointment.weight}</p>
+        <p className="card-text"><strong>Vaccinated:</strong> {appointment.vaccinated}</p>
+        <p className="card-text"><strong>Taxi Service:</strong> {appointment.taxiService}</p>
       </div>
     </div>
   );
