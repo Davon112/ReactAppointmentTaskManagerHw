785d50f5ea995063d1f719e5bd5b25bbbdebb1a6
Da'Von Manuel <160780050+Davon112@users.noreply.github.com>
2024-07-16 09:55:20 -0500

    Update Appointments.tsx

--- src/components/MyAppointments/Appointments.tsx
+++ src/components/MyAppointments/Appointments.tsx
@@ -1,7 +1,5 @@
-
 import React from 'react';
-import AppointmentCard from '.';
-import { Appointment } from '.';
+import AppointmentCard, {Appointment} from '.';
 
 const appointments: Appointment[] = [
   {
